ddf1a3a523ff1ab3433586fd1d5887eade4beaa0
David Mazza <github@dmzza.com>
2013-07-21 08:39:10 -0400

    Find nearest station to a fixed location

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,26 @@
 
 var Citibike = require('citibike')
 	, citibike = new Citibike
-	, stations = new Array();
+	, stations = new Array()
+	, lat = 40.74144387
+	, lon = -73.97536082;
 
 exports.index = function(req, res){
 	citibike.getStations(null, function(data) {
 	  stations = data["results"];
-	  res.render('index', { title: 'CitiBike', stations: stations });
+	  closestStation = null;
+	  minDistance = 10000;
+
+	  for(var i = 0; i < stations.length; i++) {
+	  	distance = Math.abs(lat - stations[i].latitude) + Math.abs(lon - stations[i].longitude)
+	  	if(distance < minDistance) {
+	  		minDistance = distance;
+	  		closestStation = stations[i];
+	  	}
+	  }
+
+
+	  res.render('index', { title: 'CitiBike', stations: stations, closestStation: closestStation });
 	});
   //res.render('index', { title: 'CitiBike', stations: stations });
 

7d0d834c80303729a045c2eccdd748e7c5e107d9
David Mazza <github@dmzza.com>
2013-07-26 13:13:28 -0400

    Updates work, front-end still broken

--- routes/index.js
+++ routes/index.js
@@ -17,9 +17,10 @@ var mongoUri = process.env.MONGOLAB_URI ||
 
 exports.index = function(req, res){
 	citibike.getStations({updateOnly: "true"}, function(data) {
-	  stations = data["results"];
-	  timestamp = data["lastUpdate"];
-	  allStations = new Array();
+	  var stations = data["results"]
+	  	, timestamp = data["lastUpdate"]
+	  	, allStations = new Array()
+	  	, output = new Array();
 
 		mongo.Db.connect(mongoUri, function (err, db) {
 			if (err) throw err;
@@ -36,30 +37,27 @@ exports.index = function(req, res){
 					if (err) throw err;
 
 					var thisStation = allStations[stationRecord["id"]];
-					console.log(thisStation);
 
 					stationRecord["availableBikes"] = thisStation["availableBikes"];
 					stationRecord["availableDocks"] = thisStation["availableDocks"];
-					//stationRecord["status"] = station["status"];
-					//allStations.push(stationRecord);
+					stationRecord["status"] = thisStation["status"];
+					output.push(stationRecord);
 
 					updateCollection.findOne({"id": stationRecord["id"], "lastUpdate": stationRecord["lastUpdate"]}, function(err, lastUpdate) {
 						if (err) throw err;
 
-						//console.log(station);
-						if(lastUpdate == null || lastUpdate["availableBikes"] != station["availableBikes"]) {
+						if(lastUpdate == null || lastUpdate["availableBikes"] != stationRecord["availableBikes"]) {
 							updateCollection.insert(allStations[stationRecord["id"]], {safe: true}, function(err,response) {
 								if (err) throw err;
-								//console.log(station);
 							});
 							stationCollection.update({"id": stationRecord["id"]}, {$set: {"lastUpdate": timestamp}});
 						}
 					});
 				});
-				//if(allStations.length === stations.length) {
-					res.render('index', { title: 'CitiBike', stations: allStations });
-				//}
 			}
+			setTimeout(function() {
+				res.render('index', { title: 'CitiBike', stations: output });
+			}, 2000);
 		});
 
 

be86b0780c852cff730e174474193c2cea3ba3df
David Mazza <github@dmzza.com>
2013-07-26 12:02:31 -0400

    Updates collection WIP

--- routes/index.js
+++ routes/index.js
@@ -18,28 +18,51 @@ var mongoUri = process.env.MONGOLAB_URI ||
 exports.index = function(req, res){
 	citibike.getStations({updateOnly: "true"}, function(data) {
 	  stations = data["results"];
-	  timestamp = data["lastUpdate"]
+	  timestamp = data["lastUpdate"];
+	  allStations = new Array();
 
 		mongo.Db.connect(mongoUri, function (err, db) {
 			if (err) throw err;
-			console.log("Connected to Database");
 
-			//simple json record
-			var document = {name:"David", title:"About MongoDB"};
-			db.collection('stations', function(er, collection) {
-				for(i=0; i<stations.length; i++) {
-					station = stations[i];
-					station["lastUpdate"] = timestamp;
-					// delete station["nearbyStations"];
-					// delete station[""]
-					collection.insert(station, {safe: true}, function(error,response) {
+			var stationCollection = db.collection('stations')
+				, updateCollection = db.collection('updates');
 
+			for(var i = 0; i<stations.length; i++) {
+				var station = stations[i];
+				station["lastUpdate"] = timestamp;
+				allStations[station["id"]] = station;
+
+				stationCollection.findOne({"id": station["id"]}, function(err, stationRecord) {
+					if (err) throw err;
+
+					var thisStation = allStations[stationRecord["id"]];
+					console.log(thisStation);
+
+					stationRecord["availableBikes"] = thisStation["availableBikes"];
+					stationRecord["availableDocks"] = thisStation["availableDocks"];
+					//stationRecord["status"] = station["status"];
+					//allStations.push(stationRecord);
+
+					updateCollection.findOne({"id": stationRecord["id"], "lastUpdate": stationRecord["lastUpdate"]}, function(err, lastUpdate) {
+						if (err) throw err;
+
+						//console.log(station);
+						if(lastUpdate == null || lastUpdate["availableBikes"] != station["availableBikes"]) {
+							updateCollection.insert(allStations[stationRecord["id"]], {safe: true}, function(err,response) {
+								if (err) throw err;
+								//console.log(station);
+							});
+							stationCollection.update({"id": stationRecord["id"]}, {$set: {"lastUpdate": timestamp}});
+						}
 					});
-				}
-			});
+				});
+				//if(allStations.length === stations.length) {
+					res.render('index', { title: 'CitiBike', stations: allStations });
+				//}
+			}
 		});
 
-		res.render('index', { title: 'CitiBike', stations: stations });
+
 	});
 
 };

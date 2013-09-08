1fb6e92a2a19a6cdd0a296ca3a253c7a9144b636
David Mazza <github@dmzza.com>
2013-09-08 00:14:45 -0400

    Refactoring now makes /sync work, just returning the CitiBike Station API output

--- routes/station.js
+++ routes/station.js
@@ -3,7 +3,9 @@
  * GET stations listing.
  */
 
-var mongo = require('mongodb');
+var Citibike = require('citibike')
+	, citibike = new Citibike
+	, mongo = require('mongodb');
 
 var mongoUri = process.env.MONGOLAB_URI ||
 		process.env.MONGOHQ_URL ||
@@ -118,5 +120,45 @@ exports.list = function(req, res){
 			console.log(err);
 			res.send(500);
 		}
+	});
+};
+
+exports.sync = function(req, res){
+	citibike.getStations(null, function(data) {
+	  var stations = data["results"]
+	  	, timestamp = data["lastUpdate"]
+	  	, allStations = new Array()
+	  	, output = new Array();
+
+	    mongo.Db.connect(mongoUri, function (err, db) {
+	      try {
+	      if (err) throw err;
+
+	      var stationCollection = db.collection('stations')
+	        , updateCollection = db.collection('updates');
+
+	      for(var i = 0; i<stations.length; i++) {
+	        var station = stations[i];
+
+	        stationCollection.findOne({"id": station["id"]}, function(err, stationRecord) {
+	          try {
+	          if (err) throw err;
+	          if(stationRecord === null) throw "missing station";
+
+	          // TODO: UPDATE OR CREATE EACH STATION HERE
+
+	          } catch(err) {
+	          console.log(err);
+	          res.send(500);
+	          }
+	        });
+	      }
+
+	      } catch(err) {
+	      console.log(err);
+	      res.send(500);
+	      }
+	    });
+    	res.send(stations);
 	});
 };
